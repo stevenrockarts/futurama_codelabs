b58c74486d7abe360bcd04e18884cbe995636d4b
Steven Rockarts <steven.rockarts@thinkific.com>
2023-07-11 15:57:34 -0600

    Fix build error

--- src/riverpod_guide/01_intro/snippet.dart
+++ src/riverpod_guide/01_intro/snippet.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 //TODO: Import the riverpod package
 void main() {
   //TODO: wrap the entire app with a ProviderScope so that widgets will be able to read providers
-  runApp(
-      child: MaterialApp(
+  runApp(MaterialApp(
     home: HelloWorldWidget(),
   ));
 }
